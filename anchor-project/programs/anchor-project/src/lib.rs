acfdd79b8faf1293e2d00e6588088a54a7168ddc
OnlyForward0613 <harryborison613@gmail.com>
2022-08-25 18:20:02 +0800

    Added base code for Rust

--- anchor-project/programs/anchor-project/src/lib.rs
+++ anchor-project/programs/anchor-project/src/lib.rs
@@ -3,13 +3,64 @@ use anchor_lang::prelude::*;
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
 #[program]
-pub mod anchor_project {
+pub mod myepicproject {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn start_stuff_off(ctx: Context<StartStuffOff>) -> Result<()> {
+        // Get a reference to the account.
+        /*
+            Note: We do &mut to get a "mutable reference" to base_account. When we do this it actually gives us the power to make changes to base_account. Otherwise, we'd simply be working w/ a "local copy" of base_account.
+        */
+        let base_account = &mut ctx.accounts.base_account;
+
+        // Initialize total_gifs.
+        base_account.total_gifs = 0;
+        Ok(())
+    }
+
+    // Another function woo!
+    pub fn add_gif(ctx: Context<AddGif>) -> Result<()> {
+        // Get a reference to the account and increment total_gifs.
+        let base_account = &mut ctx.accounts.base_account;
+        base_account.total_gifs += 1;
         Ok(())
     }
 }
 
+// Attach certain variables to the StartStuffOff context.
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct StartStuffOff<'info> {
+    // Just initializing the BaseAccount here
+    /*
+        1. 'init' creates a new account owned by our current program.
+        2. 'payer = user' tells our program who's paying for the account to be created. In this case, it's the user calling the function.
+        3. 'space = 9000' will allocate 9000 bytes of space for our account. The more logic we add to our program, the more space we need.
+    */
+    #[account(init, payer = user, space = 9000)]
+    pub base_account: Account<'info, BaseAccount>,
+
+    // "pub user: Signer<'info>" is data passed into the program that proves to the program that the user calling this program actually owns their wallet account.
+    #[account(mut)]
+    pub user: Signer<'info>,
+
+    // 'system_program: Program' is basically a reference to the SystemProgram, which is the program that basically runs Solana. It is responsible for a lot of stuff, but one of the main things it does is create accounts on Solana. The SystemProgram is a program the creators of Solana deployed that other programs like ours talk to — it has an id of 11111111111111111111111111111111.
+    pub system_program: Program<'info, System>,
+}
+
+// Specify what data you want in the AddGif Context.
+#[derive(Accounts)]
+pub struct AddGif<'info> {
+    /* 
+        Creates a Context named AddGif that has access to a mutable reference to base_account. "#[account(mut)]" basically means we can actually change the total_gifs value stored on BaseAccount.
+
+        Otherwise, it may change data on it within the function but it wouldn't actually change on account. Now, w/ a "mutable" reference if we edit w/ base_account in the function it'll change data on the account itself. 
+    */
+    #[account(mut)]
+    pub base_account: Account<'info, BaseAccount>,
+}
+
+// Tell Solana what we want to store on this account.
+#[account]
+pub struct BaseAccount {
+    pub total_gifs: u64,
+}
--- anchor-project/tests/anchor-project.js
+++ anchor-project/tests/anchor-project.js
@@ -1,13 +1,60 @@
 const anchor = require("@project-serum/anchor");
 
-describe("anchor-project", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+// Need the system program, will talk about this soon.
+const { SystemProgram } = anchor.web3;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const program = anchor.workspace.AnchorProject;
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+const main = async () => {
+  console.log("🚀 Starting test...");
+
+  // Create and set the provider. We set it before but we needed to update it, so that it can communicate with our frontend!
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
+
+  const program = anchor.workspace.Myepicproject;
+
+  // Create an account keypair for our program to use.
+  const baseAccount = anchor.web3.Keypair.generate();
+
+  // Call start_stuff_off, pass it the params it needs!
+  /* 
+    Note: notice also that in lib.rs the function is called start_stuff_off since in Rust we use snake case (snake_case) instead of camel case. But, over in our javascript file we use camel case and actually call startStuffOff. This is something nice Anchor does for us so we can follow best practices regardless of what language we're using. 
+  */
+  let tx = await program.rpc.startStuffOff({
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+      user: provider.wallet.publicKey,
+      systemProgram: SystemProgram.programId,
+    },
+    signers: [baseAccount],
   });
-});
+
+  console.log("📝 Your transaction signature", tx);
+
+  // Fetch data from the account.
+  // Here we actually retrieve the account we created and then access totalGifs
+  let account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+  console.log("👀 GIF Count", account.totalGifs.toString());
+
+  // Call add_gif!
+  await program.rpc.addGif({
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+    },
+  });
+
+  // Get the account again to see what changed.
+  account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+  console.log("👀 GIF Count", account.totalGifs.toString());
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+runMain();
